980694826cca3f5475683132c54e4437cb4a3a78
Young Kuo <tw01472760@gmail.com>
2021-06-22 21:02:31 +0800

    first commit

---
+++ react/todo-list/src/Components/TodoTask.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { ITask } from '../interface';
+
+interface Props {
+    task: ITask;
+    completeTask(taskName: string): void;
+}
+
+const TodoTask = ({ task, completeTask }: Props) => {
+    return (
+        <div className="task">
+            <div className="content">
+                <span>{task.taskName}</span>
+                <span>{task.deadline}</span>
+            </div>
+            <button onClick={() => {
+                completeTask(task.taskName);
+            }}>X</button>
+        </div>
+    );
+}
+
+
+export default TodoTask;
